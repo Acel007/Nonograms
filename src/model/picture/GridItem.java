d36cbd412e4578c9e620772eae918b843413bf8e
Acel <xxacel007xx@gmail.com>
2019-12-28 00:52:17 +0100

    Added automated testing for picture creation

--- src/model/picture/GridItem.java
+++ src/model/picture/GridItem.java
@@ -1,5 +1,7 @@
 package model.picture;
 
+import config.Config;
+
 public class GridItem {
 	
 	private int lengthIndex;
@@ -36,5 +38,26 @@ public class GridItem {
 
 	public void setFilled(boolean isFilled) {
 		this.isFilled = isFilled;
+	}
+	
+	@Override
+	public String toString() {
+		if(isFilled) return Config.FILLED_GRIDITEM_CHAR + "";
+		return Config.EMPTY_GRIDITEM_CHAR + "";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		try {
+			GridItem comparedObject = (GridItem)obj;
+			return lengthIndex 
+					== comparedObject.lengthIndex
+					&& heightIndex == comparedObject.heightIndex
+					&& isFilled == comparedObject.isFilled;
+		} catch(ClassCastException e) {
+			e.printStackTrace();
+			return false;
+		}
+		
 	}
 }
--- src/model/picture/PictureGrid.java
+++ src/model/picture/PictureGrid.java
@@ -1,10 +1,11 @@
 package model.picture;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PictureGrid {
 
-	private List<List<GridItem>> pictureGrid;
+	private List<List<GridItem>> pictureGrid = new ArrayList<>();
 	
 	public PictureGrid(List<List<GridItem>> pictureGrid) {
 		this.pictureGrid.addAll(pictureGrid);
