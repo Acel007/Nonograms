c4034c562a69bf05994bc6ee0cb70a8484e19b0a
Acel <xxacel007xx@gmail.com>
2019-12-30 23:09:45 +0100

    Created skeleton functions for PictureDeclarator

--- src/processing/PictureDeclarator.java
+++ src/processing/PictureDeclarator.java
@@ -1,5 +1,64 @@
 package processing;
 
-public class PictureDeclarator {
+import model.declarator.ColumnDeclarator;
+import model.declarator.Declarator;
+import model.declarator.RowDeclarator;
+import model.declarator_collection.ColumnDeclaratorCollection;
+import model.declarator_collection.RowDeclaratorCollection;
 
+public class PictureDeclarator {
+	
+	
+	
+	public RowDeclaratorCollection generateRowDeclarators() {
+		
+		
+		
+		return generateRowDeclaratorCollection();
+	}
+	
+	public ColumnDeclaratorCollection generateColumnDeclarators() {
+		
+		
+		
+		return generateColumnDeclaratorCollection();
+	}
+	
+	private RowDeclaratorCollection generateRowDeclaratorCollection() {
+		
+		
+		
+		return null;
+	}
+	
+	private ColumnDeclaratorCollection generateColumnDeclaratorCollection() {
+		
+		
+		
+		return null;
+	}
+	
+	private RowDeclarator generateRowDeclarator() {
+		
+		
+		
+		return null;
+	}
+	
+	private ColumnDeclarator generateColumnDeclarator() {
+		
+		
+		
+		return null;
+	}
+	
+	private Declarator generateDeclarator() {
+		
+		
+		
+		return null;
+	}
+	
+	
+	
 }
